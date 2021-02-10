a6c65bd422980a29fbdcbb566ee5d07e9191d93f
Michael Almond <malmond@ualberta.ca>
2021-02-09 19:59:02 -0700

    Added color
    It's purple.

--- WillWeSuriveAndroidStudio/app/src/main/java/com/example/willwesuriveandroidstudio/Polygon.java
+++ WillWeSuriveAndroidStudio/app/src/main/java/com/example/willwesuriveandroidstudio/Polygon.java
@@ -1,9 +1,7 @@
 package com.example.willwesuriveandroidstudio;
 
 public class Polygon extends Shape{
-<<<<<<< HEAD
+
     String color = "purple";
-=======
-    String color = "purple";
->>>>>>> 4c09cd4fd1c6d986b11e55184b6d93c44cbc0da6
+
 }
